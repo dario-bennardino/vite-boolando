ce09ebcc6586295bbc550d749f38fd305621dd16
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-03-21 16:01:24 +0100

    [add] creato componente Header

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+<template>
+    <header>
+        <div class="container-homepage">
+            <div class="topbar">
+                <div class="genere">
+                    <a href="">Donna</a>
+                    <a href="">Uomo</a>
+                    <a href="">Bambino</a>
+                </div>
+                <div class="logo">
+                    <img src="img/boolean-logo.png" alt="">
+                </div>
+                <div class="social">
+                    <i class="fa-regular fa-user"></i>
+                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-bag-shopping"></i>
+                </div>
+            </div>
+
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
