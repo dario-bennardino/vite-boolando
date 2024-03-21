5f86236600af72d5d73d4fb5df4d35859a2d7f28
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-03-21 18:09:42 +0100

    [add] caricato font awesome

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+
 import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import '@fortawesome/fontawesome-free/css/all.css' // Importa i file di stile di Font Awesome
+import '@fortawesome/fontawesome-free/js/all.js' // Importa il file JavaScript di Font Awesome
 
 createApp(App).mount('#app')
 import 'bootstrap/dist/js/bootstrap.js'
